cf0cfdee49bf558668c80afb3af1d577f4052726
ObamaTheLlama114 <william.chomel@gmail.com>
2023-01-27 08:09:34 -0600

    added-quiet-mode

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 use std::io::Error;
 
 use async_recursion::async_recursion;
-use clap::{command, Arg};
+use clap::{command, Arg, ArgAction};
 
 mod build;
 
 #[tokio::main]
 async fn main() {
     let matches = command!()
-        .arg(Arg::new("step"))
-        .arg(Arg::new("build_file").short('f').long("file"))
+        .arg(Arg::new("step").help("Step to run"))
+        .arg(
+            Arg::new("build_file")
+                .short('f')
+                .long("file")
+                .help("Build file to use"),
+        )
+        .arg(
+            Arg::new("quiet")
+                .short('q')
+                .long("quiet")
+                .help("Quiet mode")
+                .action(ArgAction::SetTrue),
+        )
         .get_matches();
     let step = matches.get_one::<String>("step").cloned();
     let build_file = matches.get_one::<String>("build_file").cloned();
+    let quiet = matches.get_flag("quiet");
     let step = build::get_step(step, &build_file.unwrap_or_else(|| ".".to_string()));
     match step {
-        Ok(step) => run_step(step).await.unwrap(),
+        Ok(step) => run_step(step, quiet).await.unwrap(),
         Err(error) => {
             match error {
                 build::BuildError::IoError(error) => println!("{}", error),
@@ -30,14 +43,14 @@ async fn main() {
 }
 
 #[async_recursion]
-async fn run_step(step: build::Step) -> Result<(), Error> {
+async fn run_step(step: build::Step, quiet: bool) -> Result<(), Error> {
     let mut handles = vec![];
     // Run dependencies
     for dependency in &step.dependencies {
         if dependency.asynch && !step.in_order {
-            handles.push(tokio::spawn(run_step(dependency.clone())));
+            handles.push(tokio::spawn(run_step(dependency.clone(), quiet)));
         } else {
-            run_step(dependency.clone()).await?;
+            run_step(dependency.clone(), quiet).await?;
         }
     }
 
@@ -71,6 +84,16 @@ async fn run_step(step: build::Step) -> Result<(), Error> {
             .arg("-c")
             .arg(command)
             .current_dir(dir)
+            .stdout(if quiet {
+                std::process::Stdio::null()
+            } else {
+                std::process::Stdio::inherit()
+            })
+            .stderr(if quiet {
+                std::process::Stdio::null()
+            } else {
+                std::process::Stdio::inherit()
+            })
             .spawn()?
             .wait()
             .await?;
